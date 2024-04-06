c0f974bea910fc1ae027a57d439a31ae70231c18
thenuka2004 <164596272+thenuka2004@users.noreply.github.com>
2024-04-06 20:13:32 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -23,7 +23,7 @@ global.APIs = {
 // Other
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = 'ᴠᴀᴊɪʀᴀ ʀᴀᴛʜɴᴀʏᴀᴋᴀ';
-global.owner = ['94719199757','94703475691'];
+global.owner = ['94705216555';
 global.premium = ['94719199757','94703475691'];
 global.packname = '𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢';
 global.author = '𝙑𝘼𝙅𝙄𝙍𝘼 𝙈𝘿';
